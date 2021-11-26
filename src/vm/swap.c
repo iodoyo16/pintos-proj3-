5b7c033605a8669db12af3e929ec89f2573e5ad3
iodoyo16 <durfbgksmsskfro@gmail.com>
2021-11-26 18:58:56 +0000

    swap fix

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -7,76 +7,55 @@
 static struct block *swap_block;
 static struct bitmap *available_swap;
 
-/* Page size is bigger than the sizeof swap sector.
-In order to store a page into swap, we need 'PGSIZE / BLOCK_SECTOR_SIZE' contiguous blocks. 
-Assume the size is 4096 / 512*/
+/*pagesize가 swap sector 보다 크므로 
+'PGSIZE / BLOCK_SECTOR_SIZE' 개의 contiguous blocks 이 필요하다.*/
 static const size_t SECTORS_PER_PAGE = PGSIZE / BLOCK_SECTOR_SIZE; 
 static size_t swap_size;
-
-/* Initialize the swap table. */
+// swap table 을 초기화 한다.
 void vm_swap_init(void){
-    ASSERT(SECTORS_PER_PAGE > 0);
-
-    // Initialize the swap disk
+    //ASSERT(SECTORS_PER_PAGE > 0);
+    // swap disk 초기화
     swap_block = block_get_role(BLOCK_SWAP);
-    if(swap_block == NULL){
-        PANIC("Cannot initialize the swap block");
-        NOT_REACHED();
+    if(swap_block != NULL){
+        swap_size = block_size(swap_block) / SECTORS_PER_PAGE;
+        available_swap = bitmap_create(swap_size);
+        bitmap_set_all(available_swap, true);
     }
-
-    /* Initialize the available_swap
-    each single bit of available swap corresponds to a block region.
-    It consists of contiguous SECTORS_PER_PAGE sectors, 
-    and the total size becomes PGSIZE.
-    */
-    swap_size = block_size(swap_block) / SECTORS_PER_PAGE;
-    available_swap = bitmap_create(swap_size);
-    bitmap_set_all(available_swap, true);
 }
-
-/* swap out : write the content of page into the swap disk
-and return the index of swap region. */
+/* swap table에 mapping 된 swap disk 영역의 data를 memory로 load한다. (swap in)*/
+void vm_swap_in(swap_index_t swap_idx, void *page){
+    if(!bitmap_test(available_swap, swap_idx)){
+        size_t i=0;
+        while(i < SECTORS_PER_PAGE){
+            size_t s=swap_idx * SECTORS_PER_PAGE + i;
+            void* e=page + (BLOCK_SECTOR_SIZE * i);
+            block_read(swap_block,s, e);
+            i++;
+        }
+        bitmap_set(available_swap, swap_idx, true);// 이제 available
+    }
+}
+/*page를 swap disk 에 write 하고 그 index를 반환 한다. (swap out) */
 swap_index_t vm_swap_out(void *page){
-    //Ensure the page is on users' virtual memory
-    ASSERT(page >= PHYS_BASE);
-
-    //Find available block region to use
-    size_t swap_index = bitmap_scan(available_swap, 0 , 1, true);
-
-    size_t i;
-    for(i = 0;i < SECTORS_PER_PAGE;i++){
-        //block_write(block, sector number, target address)
-        block_write(swap_block, swap_index * SECTORS_PER_PAGE + i, page+(BLOCK_SECTOR_SIZE * i));
+    //ASSERT(page >= PHYS_BASE);
+    size_t swap_idx = bitmap_scan(available_swap, 0 , 1, true); // 가능한 region을 scan.
+    size_t i = 0;
+    while(i < SECTORS_PER_PAGE){
+        size_t e = swap_idx * SECTORS_PER_PAGE + i;
+        void* s= page+(BLOCK_SECTOR_SIZE * i);
+        block_write(swap_block,e,s );
+        i++;
     }
-    //occupy the slot
-    bitmap_set(available_swap, swap_index, false);
-    return swap_index;
+    bitmap_set(available_swap, swap_idx, false); // not available 표시
+    return swap_idx;
 }
 
-/* swap in : read the content through the index from the mapped block
-and store into page. */
-void vm_swap_in(swap_index_t swap_index, void *page){
-    ASSERT(page>= PHYS_BASE);
-
-    //check the swap region
-    ASSERT(swap_index < swap_size);
-    if(bitmap_test(available_swap, swap_index) == true){
-        PANIC("Invalid read access to unassinged swap block");
-    }
-
-    size_t i;
-    for(i = 0;i < SECTORS_PER_PAGE;i++){
-        block_read(swap_block,swap_index * SECTORS_PER_PAGE + i, page + (BLOCK_SECTOR_SIZE * i));
-    }
-    bitmap_set(available_swap, swap_index, true);
-}
 
 /* Free the swap region. */
 void vm_swap_free(swap_index_t swap_index){
-    ASSERT(swap_index < swap_size);
-    if(bitmap_test(available_swap, swap_index) == true){
-        PANIC("Invalid read access to unassinged swap block");
+    //ASSERT(swap_index < swap_size);
+    if(!bitmap_test(available_swap, swap_index)){
+        bitmap_set(available_swap, swap_index, true);
     }
-    bitmap_set(available_swap, swap_index, true);
 }
 
